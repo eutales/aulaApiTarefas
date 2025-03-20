004d008b02745e56293806607f62d33a8137bc3e
tales.siqueira <eu.tsiqueira@gmail.com>
2025-03-20 14:40:29 -0300

    Finalizado

---
+++ aulaApiTarefas/Controllers/TarefaController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using aulaApiTarefas.Models;
+using aulaApiTarefas.Data;
+using System.Collections.Generic;
+
+namespace aulaApiTarefas.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TarefaController : ControllerBase
+    {
+        private readonly TarefaRepository _tarefaRepository;
+
+        public TarefaController(TarefaRepository tarefaRepository)
+        {
+            _tarefaRepository = tarefaRepository;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Tarefa>> Get()
+        {
+            return Ok(_tarefaRepository.GetAll());
+        }
+
+        [HttpPost]
+        public ActionResult Post([FromBody] Tarefa tarefa)
+        {
+            _tarefaRepository.Add(tarefa);
+            return CreatedAtAction(nameof(Get), new { id = tarefa.Id }, tarefa);
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult Put(int id, [FromBody] Tarefa tarefa)
+        {
+            tarefa.Id = id;
+            _tarefaRepository.Update(tarefa);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            _tarefaRepository.Delete(id);
+            return NoContent();
+        }
+    }
+}
---
+++ aulaApiTarefas/Data/TarefaRepository.cs
@@ -0,0 +1,77 @@
+﻿using aulaApiTarefas.Models;
+using MySql.Data.MySqlClient;
+using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
+
+
+namespace aulaApiTarefas.Data
+{
+    public class TarefaRepository
+    {
+        private readonly string _connectionString;
+
+        public TarefaRepository(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        public List<Tarefa> GetAll()
+        {
+            List<Tarefa> tarefas = new List<Tarefa>();
+
+            using (var connection = new MySqlConnection(_connectionString))
+            {
+                connection.Open();
+                var command = new MySqlCommand("SELECT * FROM Tarefas", connection);
+                var reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    tarefas.Add(new Tarefa
+                    {
+                        Id = reader.GetInt32("Id"),
+                        Descricao = reader.GetString("Descricao"),
+                        Concluida = reader.GetBoolean("Concluida")
+                    });
+                }
+            }
+
+            return tarefas;
+        }
+
+        public void Add(Tarefa tarefa)
+        {
+            using (var connection = new MySqlConnection(_connectionString))
+            {
+                connection.Open();
+                var command = new MySqlCommand("INSERT INTO Tarefas (Descricao, Concluida) VALUES (@Descricao, @Concluida)", connection);
+                command.Parameters.AddWithValue("@Descricao", tarefa.Descricao);
+                command.Parameters.AddWithValue("@Concluida", tarefa.Concluida);
+                command.ExecuteNonQuery();
+            }
+        }
+
+        public void Update(Tarefa tarefa)
+        {
+            using (var connection = new MySqlConnection(_connectionString))
+            {
+                connection.Open();
+                var command = new MySqlCommand("UPDATE Tarefas SET Descricao = @Descricao, Concluida = @Concluida WHERE Id = @Id", connection);
+                command.Parameters.AddWithValue("@Descricao", tarefa.Descricao);
+                command.Parameters.AddWithValue("@Concluida", tarefa.Concluida);
+                command.Parameters.AddWithValue("@Id", tarefa.Id);
+                command.ExecuteNonQuery();
+            }
+        }
+
+        public void Delete(int id)
+        {
+            using (var connection = new MySqlConnection(_connectionString))
+            {
+                connection.Open();
+                var command = new MySqlCommand("DELETE FROM Tarefas WHERE Id = @Id", connection);
+                command.Parameters.AddWithValue("@Id", id);
+                command.ExecuteNonQuery();
+            }
+        }
+    }
+}
